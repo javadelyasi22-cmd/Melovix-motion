a4ad8f4d7e64e75c44e6a7c44fa9efb9b7ca9beb
javadelyasi22-cmd <javadelyasi22@gmail.com>
2025-12-14 22:38:42 +0330

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontSize: 24 }}>
+      <h1>Melovix Motion</h1>
+      <p>Next.js 14 is running successfully ✅</p>
+    </main>
+  );
+                 }
