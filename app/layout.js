b66cfbe84fe61c1e930a7c958f90b72626172f89
javadelyasi22-cmd <javadelyasi22@gmail.com>
2025-12-14 22:53:07 +0330

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Melovix Motion",
+  description: "Next.js 14 App Router setup",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
