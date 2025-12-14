5527be872b7bb1c039fe0ce98e4e146c6ea965ae
javadelyasi22-cmd <javadelyasi22@gmail.com>
2025-12-14 22:49:01 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: 40, fontSize: 24 }}>
-      <h1>Melovix Motion</h1>
-      <p>Next.js 14 is running successfully ✅</p>
+    <main style={{ padding: 40, fontSize: 20 }}>
+      <h1>Melovix Motion ✅</h1>
+      <p>Next.js 14 is running successfully</p>
     </main>
   );
-                 }
+}
